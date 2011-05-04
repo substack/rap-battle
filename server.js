b778619a080224b2f8bab2be447193abe5c29185
James Halliday <mail@substack.net>
2011-05-04 06:49:33 -0700

    untested server

---
+++ server.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var app = express.createServer();
+app.use(express.static(__dirname + '/static'));
+app.listen(8080);
+
+var browserify = require('browserify');
+app.use(browserify({
+    require : [ 'dnode' ]
+}));
+
+var EventEmitter = require('events').EventEmitter;
+var Hash = require('hashish');
+var Seq = require('seq');
+var dnode = require('dnode');
+
+var watchers = {};
+var competitors = {};
+
+var contest = {
+    emit : function () {
+        var args = arguments;
+        Hash(watchers).forEach(function (w) {
+            w.apply(null, args);
+        });
+    },
+    round : function (cb) {
+        Seq.ap(competitors)
+            .seqEach_(function (next, challenge) {
+                challenge(function (msg) {
+                    context.emit(msg);
+                    next();
+                });
+            })
+            .seq(cb)
+        ;
+    },
+};
+
+dnode(function (client, conn) {
+    this.watch = function (emit) {
+        watchers[conn.id] = emit;
+    };
+    
+    this.rap = function (challenge, cb) {
+        if (competitors[client.name]) {
+            cb('A client by that name is already competing');
+        }
+        else {
+            competitors[client.name] = challenge;
+            
+            conn.on('end', function () {
+                contest.emit('quit', client.name);
+            });
+        }
+    };
+    
+    this.battle = function (secret, cb) {
+        if (secret !== 'secretsauce') {
+            cb('ACCESS DENIED');
+        }
+        else if (Hash(competitors).length < 2) {
+            cb('Not enough competitors');
+        }
+        else {
+            Seq.ap(Array(5)).seqEach(function () {
+                contest.round(this.ok);
+            });
+            cb(null);
+        }
+    };
+}).listen(8081).listen(app);
